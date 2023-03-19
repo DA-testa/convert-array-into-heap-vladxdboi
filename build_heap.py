15c59f4233b61b2de83174503ebc00949be521f6
vladxdboi <vladiks232@inbox.lv>
2023-03-19 16:11:38 +0200

    obamium

--- build_heap.py
+++ build_heap.py
@@ -1,38 +1,52 @@
 # python3
+# Vladislavs Senevičs, 11.grupa, 221RDB453
 
+import os
 
 def build_heap(data):
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
+    n = len(data)
 
+    for i in range(n//2, -1, -1):
+        j = i
+        while True:
+            left = 2 * j + 1
+            right = 2 * j + 2
+            min = j
+
+            if left < n and data[left] < data[min]:
+                min = left
+
+            elif right < n and data[right] < data[min]:
+                min = right
+
+            elif min == j:
+                break
+
+            swaps.append((j, min))
+            data[j], data[min] = data[min], data[j]
+            j = min
 
     return swaps
 
-
 def main():
+    input_type = input("Enter input type: ")
+
+    if input_type == 'I':
+        n = int(input())
+        data = list(map(int, input().split()))
+    elif input_type == 'F':
+        path = input("Enter file path: ").strip()
+        with open(path, 'r') as file:
+            n = int(file.readline().strip())
+            data = list(map(int, file.readline().strip().split()))
+    else:
+        print('Invalid input type')
+        return
     
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
-
-
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
-
-    # checks if lenght of data is the same as the said lenght
     assert len(data) == n
 
-    # calls function to assess the data 
-    # and give back all swaps
     swaps = build_heap(data)
-
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
